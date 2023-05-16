5eb4c6958873284a0eed89e746368d0b12901870
ReemaDambre <reemachavhan999@gmail.com>
2023-05-16 14:51:30 +0530

    Test commit

---
+++ Jenkin_Assignment_01/src/Engine/Priority.java
@@ -0,0 +1,35 @@
+package Engine;
+
+import org.testng.annotations.Test;
+
+public class Priority {
+
+	@Test(priority = 1)
+	public void StartEngine() {
+		System.out.println("Engine Started");
+		
+		
+	}
+	@Test(priority =2)
+	public void putFirstGear()
+	{
+		System.out.println("Car is in first Gear");
+		
+	}
+	@Test(priority =3)
+	public void putSecondGear() {
+		System.out.println("Car is in second Gear");
+		
+	}
+	
+	@Test(priority = 4) 
+	public void putThirdGear() {
+		System.out.println(" Car is in third Gear");
+		
+		
+	}
+	
+	
+	
+	
+}
